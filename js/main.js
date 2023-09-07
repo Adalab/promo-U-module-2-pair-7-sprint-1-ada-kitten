af42ebc45c5e9997e40d18bd2b0d9c76b3677679
ysabelvalencia <ysavalencia@hotmail.com>
2023-09-07 11:19:13 +0200

    update day two

--- js/main.js
+++ js/main.js
@@ -1,49 +1,76 @@
 "use strict";
+
 const listElement = document.querySelector(".js-list");
-/* Agregar el código del li desde HTMl 
-Repetir este proceso por cada gatito */
+
+const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName1  = 'Anastacio';
+// kittenName1.innerHTML = kittenName1.toUpperCase();
+// kittenName1MAYUS = kittenName1.toUpperCase();
+// System.out.println(kittenName1MAYUS);
+const kittenDesc1 =
+  ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRace1= 'Siamés';
+
+
+
 const kittenOne = `<li class="card">
 <article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
+  <img class="card_img" src="${kittenImage1}"
     alt="gatito"
   />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
+  <h3 class="card_title">"${kittenName1}</h3>
+  <h4 class="card_race">"${kittenRace1}"</h4>
+  <p class="card_description">"${kittenDesc1}
    </p>
 </article>
 </li>`;
+
+
+const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenName2 = 'Fiona';
+const kittenDesc2 =
+  'Produce fascinación y curiosidad. Exótico, raro, bello, extraño…hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenRace2= 'Sphynx';
+
 const kittenTwo = `<li class="card">
               <img
                 class="card_img"
-                src="https://dev.adalab.es/sphynx-gato.webp"
+                src="${kittenImage2}"
                 alt="sphynx-cat"
               />
-              <h3 class="card_title">Fiona</h3>
-              <h4 class="card_race">Sphynx</h4>
-              <p class="card_description">
-                Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-                hasta con pinta de alienígena han llegado a definir a esta raza
-                gatuna que se caracteriza por la «ausencia» de pelo.
+              <h3 class="card_title">"${kittenName2}"</h3>
+              <h4 class="card_race">"${kittenRace2}</h4>
+              <p class="card_description">"${kittenDesc2}
               </p>
           </li>`;
+
+const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName3 = 'Cielo';
+const kittenDesc3 =
+  'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su  bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenRace3= 'Maine Coon';
+
+
+
 const kittenThree = `<li class="card">
               <img
-                class="card_img"
-                src="https://dev.adalab.es/maine-coon-cat.webp"
+              class="card_img"
+              src= '${kittenImage3}'
                 alt="maine-coon-cat"
               />
-              <h3 class="card_title">Cielo</h3>
-              <h4 class="card_race">Maine Coon</h4>
-              <p class="card_description">
-                Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-                bella mirada se ha convertido en una de sus señas de identidad.
-                Sus ojos son grandes y las orejas resultan largas y en punta.
+              <h3 class="card_title">"${kittenName3}"</h3>
+              <h4 class="card_race">"${kittenRace3}</h4>
+              <p class="card_description">"${kittenDesc3}
               </p>
           </li>`;
+
+
 listElement.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+// KittenName1 = 'Anastacio';
+
+
+         
+
+console.log(kittenName1.toUpperCase());
+
